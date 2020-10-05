bb07c77116dc0ced144c5207a2d176e1dbd758b6
Yash <yashrajmehta3027@gmail.com>
2020-10-05 10:30:12 +0530

    Initial commit

---
+++ app/src/main/java/com/rajaryan/customtoast/MainActivity.java
@@ -0,0 +1,20 @@
+package com.rajaryan.customtoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.muddzdev.styleabletoast.StyleableToast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void showToast(View v) {
+        StyleableToast.makeText(this, "Hello World!", R.style.exampleToast).show();
+    }
+}
